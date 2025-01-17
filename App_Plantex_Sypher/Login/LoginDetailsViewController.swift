a320a6b481c2177c6aa9c27eeb1748d2b49e92f1
hardikb1111 <hardikbhardwaj110605@gmail.com>
2025-01-17 15:33:54 +0530

    ammended

--- App_Plantex_Sypher/Login/LoginDetailsViewController.swift
+++ App_Plantex_Sypher/Login/LoginDetailsViewController.swift
@@ -27,10 +27,33 @@ class LoginDetailsViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+       
     }
     
     @IBAction func onRegisterTapped(_ sender: UIButton) {
+        guard let name = nameTxt.text, !name.isEmpty,
+                  let phone = phoneTxt.text, !phone.isEmpty,
+                  let email = emailTxt.text, !email.isEmpty,
+                  let password = PassWordText.text, !password.isEmpty,
+                  let confirmPassword = cnfPassWordText.text, !confirmPassword.isEmpty,
+                  let userImageData = userImage.image?.jpegData(compressionQuality: 0.8) else {
+                print("All fields are required.")
+                return
+            }
         
+        
+        let id = UUID()
+            let imageBase64 = userImageData.base64EncodedString()
+
+            UserDataModel.shared.addUser(
+                id: id,
+                name: name,
+                email: email,
+                password: password,
+                confirmPassword: confirmPassword,
+                phoneNumber: phone,
+                image: imageBase64
+            )
         
     }
     
