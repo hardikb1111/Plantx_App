5c6d4b5b23476cbe83ee60d418a54f9412935b8d
Durgesh1008 <dk1570801@gmail.com>
2025-01-26 12:51:55 +0530

    Home Page done

---
+++ App_Plantex_Sypher/Home/ButtonGridCell.swift
@@ -0,0 +1,12 @@
+//
+//  ButtonGridCell.swift
+//  App_Plantex_Sypher
+//
+//  Created by DIWAKAR KUMAR on 26/01/25.
+//
+
+import UIKit
+
+class ButtonGridCell: UICollectionViewCell {
+    
+}
---
+++ App_Plantex_Sypher/Home/CareButtonTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CareButtonTableViewCell.swift
+//  App_Plantex_Sypher
+//
+//  Created by DIWAKAR KUMAR on 26/01/25.
+//
+
+import UIKit
+
+class CareButtonTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
